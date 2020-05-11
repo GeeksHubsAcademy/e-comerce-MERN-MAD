0280aa5e7196145a99d80f18f56c17d69aeb6464
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-05-11 16:43:43 +0200

    finished user-redux

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 import { connect } from 'react-redux'
+import { logout } from '../redux/actions/users';
 const Header = ({ user }) => {
-    const logout = ()=>{
-        
-    }
+    
     return (
         <header className="header">
             <NavLink to='/' exact>Home</NavLink>
             {user ?
                 <div className="userZone">
-
                     <NavLink to='/profile' exact>{user.email}</NavLink>
                     <div onClick={logout}>Logout</div>
                 </div>
