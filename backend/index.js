5db18362c8f9a148b80f43afbbc76ae8448576c8
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-05-11 14:06:58 +0200

    added cors policy

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import express from 'express';
 import usersRouter from './routes/users.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
 app.use(express.json()) //parsea el json que mandamos y crea el req.body 
 app.use('/users', usersRouter);
